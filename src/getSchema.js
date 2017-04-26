b951c21f99c150b45f4623a11329d8585f7f06d2
James Lal <james@lightsofapollo.com>
2017-04-26 15:14:15 -0700

    Add handling for introspection query schema.

--- src/getSchema.js
+++ src/getSchema.js
@@ -1,25 +1,34 @@
 // @flow
 
-import { buildASTSchema, parse } from 'graphql'
+import {
+  printSchema,
+  buildClientSchema,
+  buildASTSchema,
+  parse
+} from 'graphql'
 import fs from 'fs'
+import path from 'path'
 
 import type { GraphQLSchema } from 'graphql'
 
-export default function getSchema (schemaPath: string): GraphQLSchema {
+export default function getSchema(schemaPath: string): GraphQLSchema {
   try {
     let source = fs.readFileSync(schemaPath, 'utf8')
+    if (path.extname(schemaPath) === '.json') {
+      source = printSchema(buildClientSchema(JSON.parse(source).data))
+    }
     source = `
-      directive @include(if: Boolean) on FRAGMENT | FIELD
-      directive @skip(if: Boolean) on FRAGMENT | FIELD
-      directive @relay(pattern: Boolean, plural: Boolean) on FRAGMENT | FIELD
-      ${source}
-    `
+  directive @include(if: Boolean) on FRAGMENT | FIELD
+  directive @skip(if: Boolean) on FRAGMENT | FIELD
+  directive @relay(pattern: Boolean, plural: Boolean) on FRAGMENT | FIELD
+  ${source}
+  `
+
     return buildASTSchema(parse(source))
   } catch (error) {
     throw new Error(`
-Error loading schema. Expected the schema to be a .graphql file using the
-GraphQL schema definition language. Error detail:
-
+Error loading schema. Expected the schema to be a .graphql or a .json
+file, describing your GraphQL server's API. Error detail:
 ${error.stack}
     `.trim())
   }
