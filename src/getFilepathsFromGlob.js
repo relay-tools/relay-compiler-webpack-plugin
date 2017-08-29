c977c62cce8010d90120cd0602df15606f5fa5a3
Stephen Thomas <sthomas1618@gmail.com>
2017-08-29 15:39:00 -0700

    Allow optional usage of watchman
    Port getFilepathsFromGlob from relay-compiler.
    Add watchman option.
    Useful for CI builds, where watchman may not be installed.

---
+++ src/getFilepathsFromGlob.js
@@ -0,0 +1,20 @@
+export default function getFilepathsFromGlob(
+  baseDir,
+  options: {
+    extensions: Array<string>,
+    include: Array<string>,
+    exclude: Array<string>,
+  },
+): Array<string> {
+  const {extensions, include, exclude} = options;
+  const patterns = include.map(inc => `${inc}/*.+(${extensions.join('|')})`);
+
+  // $FlowFixMe(site=react_native_fb,www)
+  const glob = require('fast-glob');
+  return glob.sync(patterns, {
+    cwd: baseDir,
+    bashNative: [],
+    onlyFiles: true,
+    ignore: exclude,
+  });
+}
