43fb603268d98d59fd6bf055fa6c2eb986e61364
Stephen Thomas <sthomas1618@gmail.com>
2017-08-29 15:39:49 -0700

    Build

--- dist/index.js
+++ dist/index.js
@@ -22,6 +22,10 @@ var _buildWatchmanExpression = require('./buildWatchmanExpression');
 
 var _buildWatchmanExpression2 = _interopRequireDefault(_buildWatchmanExpression);
 
+var _getFilepathsFromGlob = require('./getFilepathsFromGlob');
+
+var _getFilepathsFromGlob2 = _interopRequireDefault(_getFilepathsFromGlob);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
@@ -36,7 +40,8 @@ class RelayCompilerWebpackPlugin {
         getFileFilter: _getFileFilter2.default,
         getParser: _relayCompiler.FileIRParser.getParser,
         getSchema: () => {},
-        watchmanExpression: []
+        watchmanExpression: null,
+        filepaths: null
       }
     };
     this.writerConfigs = {
@@ -68,14 +73,18 @@ class RelayCompilerWebpackPlugin {
       throw new Error('Could not find your `src` path. Have you provided a fully resolved path?');
     }
 
-    this.parserConfigs.default.baseDir = options.src;
-    this.parserConfigs.default.schema = options.schema;
-    this.parserConfigs.default.getSchema = () => (0, _getSchema2.default)(options.schema);
-    this.parserConfigs.default.watchmanExpression = (0, _buildWatchmanExpression2.default)({
+    const watchman = options.watchman !== undefined ? options.watchman : true;
+    const fileOptions = {
       extensions: ['js'],
       include: ['**'],
       exclude: ['**/node_modules/**', '**/__mocks__/**', '**/__tests__/**', '**/__generated__/**']
-    });
+    };
+
+    this.parserConfigs.default.baseDir = options.src;
+    this.parserConfigs.default.schema = options.schema;
+    this.parserConfigs.default.getSchema = () => (0, _getSchema2.default)(options.schema);
+    this.parserConfigs.default.watchmanExpression = options.watchman ? (0, _buildWatchmanExpression2.default)(fileOptions) : null;
+    this.parserConfigs.default.filepaths = options.watchman ? null : (0, _getFilepathsFromGlob2.default)(options.src, fileOptions);
 
     this.writerConfigs.default.getWriter = (0, _getWriter2.default)(options.src);
 
