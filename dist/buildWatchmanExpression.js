58ea4dbb5c126b53a8bb0f3fe51c3c88c41bcfad
Joshua Peek <josh@joshpeek.com>
2017-11-29 13:27:50 -0800

    Remove unused module
    Last call site was removed in 298f2569b3940fd2a0ef62dfafe261a27ebc3418.

--- dist/buildWatchmanExpression.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = buildWatchExpression;
-function buildWatchExpression(options) {
-  return ['allof', ['type', 'f'], ['anyof', ...options.extensions.map(ext => ['suffix', ext])], ['anyof', ...options.include.map(include => ['match', include, 'wholename'])], ...options.exclude.map(exclude => ['not', ['match', exclude, 'wholename']])];
-}
