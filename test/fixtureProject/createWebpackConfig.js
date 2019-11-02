c001379ed8a2f057ab8e1703a5d11abec5a28255
Daniel Holmes <daniel@danielholmes.org>
2019-11-02 16:14:31 +1100

    Build for new version

--- test/fixtureProject/createWebpackConfig.js
+++ test/fixtureProject/createWebpackConfig.js
@@ -1,27 +1,24 @@
 const path = require('path')
 
-module.exports = ({ relayCompilerWebpackPlugin, plugins = [] }) => {
-  console.log('createWebpackConfig', __dirname)
-  return ({
-    mode: 'production',
-    context: __dirname,
-    entry: './src/entry.js',
-    output: {
-      path: path.resolve('dist'),
-      filename: 'index.js'
-    },
-    performance: {
-      hints: false
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: path.join(__dirname, 'node_modules'),
-          use: [{ loader: 'babel-loader' }]
-        }
-      ]
-    },
-    plugins: [relayCompilerWebpackPlugin, ...plugins]
-  })
-}
+module.exports = ({ relayCompilerWebpackPlugin, plugins = [] }) => ({
+  mode: 'production',
+  context: __dirname,
+  entry: './src/entry.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index.js'
+  },
+  performance: {
+    hints: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: path.join(__dirname, 'node_modules'),
+        use: [{ loader: 'babel-loader' }]
+      }
+    ]
+  },
+  plugins: [relayCompilerWebpackPlugin, ...plugins]
+})
